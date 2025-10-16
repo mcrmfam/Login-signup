62a931209698c2a092bac2c7d1d830d64b266991
mcrmfam <mcrfam@yahoo.com>
2025-10-16 10:26:29 -0500

    First commit app.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+    </div>
+  );
+}
+
+export default App;
