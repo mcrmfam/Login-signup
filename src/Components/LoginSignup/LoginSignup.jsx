4288e9ba251b29dd75994614032c3122578f9bae
mcrmfam <mcrfam@yahoo.com>
2025-10-17 10:38:37 -0500

    signup/login buttons changed to clickable plus format

--- src/Components/LoginSignup/LoginSignup.jsx
+++ src/Components/LoginSignup/LoginSignup.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignup.css';
 
 import user_icon from '../Assets/name.png';
@@ -6,10 +6,13 @@ import email_icon from '../Assets/email.png';
 import password_icon from '../Assets/password.png';
 
 const LoginSignup = () => {
+  
+  const [action, setAction] = useState("Sign Up");
+  
   return (
     <div className='container'>
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
 
@@ -35,8 +38,8 @@ const LoginSignup = () => {
       </div>
 
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
-        <div className='submit'>Login</div>
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign up</div>
+        <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div>
     </div>
   );
